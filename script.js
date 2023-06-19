69a5b40012cf82599d18a47ad45d1520eb9c9ec6
Manjeet7087 <131547570+Manjeet7087@users.noreply.github.com>
2023-06-19 21:24:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+// script.js
+const element = document.getElementById("level");
+let domLevel = 0;
+
+while (element !== document.documentElement) {
+  element = element.parentNode;
+  domLevel++;
+}
+
+alert("The level of the element is: " + domLevel);
+
