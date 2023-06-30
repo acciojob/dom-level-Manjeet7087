23709681638ca6aa74529a9cabdffad08d770c11
Manjeet7087 <131547570+Manjeet7087@users.noreply.github.com>
2023-06-30 22:48:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //your JS code here. If required.
 // script.js
-et s=document.getElementById("level").innerText;
+let s=document.getElementById("level").innerText;
 let ans=s[s.length-1];
 alert("The level of the element is: "+ans);
